af5172fc01a61d99537e9be11d90880d086a17e1
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-21 14:59:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
       FROM students
       WHERE grade <= '11'
     SQL
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
