23b6520e9fb659969d40e2f61ce48ac1ce178174
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-21 14:41:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT name
+      FROM students
+      WHERE grade = '9'
+    SQL
+    DB[:conn].execute(sql)
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
